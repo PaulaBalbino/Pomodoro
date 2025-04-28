b66c182688d767dff9131db4a8fe69743273c76f
Paula Balbino <lola.paulinha@gmail.com>
2025-04-28 20:20:17 +0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import './styles/theme.css'
+import './styles/global.css'
+
+
+import { Heading } from './components/Heading';
+import { Container } from './components/Container';
+
+
+export function App() {
+	console.log('Oi');
+
+	return(
+		<>
+
+		<Container>
+			<Heading>LOGO</Heading>
+		</Container>
+		<Container>
+			<Heading>MENU</Heading>
+		</Container>
+
+		</>
+	);
+}
