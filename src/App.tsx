5464c85a324d3c1920d40ad5e85f812dad2007eb
Paula Balbino <lola.paulinha@gmail.com>
2025-05-01 16:15:05 +0400

    add:cycles

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './styles/theme.css'
 import './styles/global.css'
 
 import { Container } from './components/Container';
+import { Cycles } from './components/Cycles';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
 import { CountDown } from './components/CountDown';
@@ -39,8 +40,9 @@ export function App() {
 				</div>
 
 				<div className='formRow'>
-					<p>Ciclos</p>
-					<p>0 0 0 0 0</p>
+					<Cycles />
+
+
 					<button>Enviar</button>
 				</div>
 			</form>
