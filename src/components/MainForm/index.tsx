11319fd7efac19bd6ccaa02ea68b76934a9e7116
Paula Balbino <lola.paulinha@gmail.com>
2025-05-22 21:58:26 +0400

    added getNextCycle and nextCycleType

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -3,13 +3,58 @@ import { Cycles } from "../Cycles";
 import { DefaultButton } from "../DefaultButton";
 import { PlayCircleIcon } from 'lucide-react';
 import { useRef } from "react";
+import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
+import { TaskModel } from "../../models/TaskModel";
+import { getNextCycle } from "../../utils/getNextCycle";
+import { getNextCycleType } from "../../utils/getNextCycleType";
+import { formatSecondsToMinutes } from '../../utils/formatSecondsToMinutes';
 
 
 export function MainForm() {
+	const { state, setState } = useTaskContext();
 	const taskNameInput = useRef<HTMLInputElement>(null);
+
+	// Cycles
+	const nextCycle = getNextCycle(state.currentCycle);
+	const nextCycleType = getNextCycleType(nextCycle);
 
 	function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
 		event.preventDefault();
+
+		if (taskNameInput.current === null)
+			return;
+
+		const taskName = taskNameInput.current.value.trim();
+		console.log(taskName);
+
+		if (!taskName) {
+			alert('Write the task name')
+			return;
+		}
+
+		const newTask: TaskModel = {
+			id: Date.now().toString(), // cria um id unico, praticamente imp criar 2 ao mesmo tempo
+			name: taskName,
+			startDate: Date.now(),
+			completeDate: null,
+			interruptDate: null,
+			duration: state.config[nextCycleType],
+			type: nextCycleType,
+		};
+
+		const secondsRemaining = newTask.duration * 60;
+
+		setState(prevState => {
+			return {
+				...prevState,
+				config: {...prevState.config},
+				activeTask: newTask,
+				currentCycle: nextCycle,
+				secondsRemaining, // nao precisa atribuir pois tem o mesmo nome da variavel
+				formattedSecondsRemaining: formatSecondsToMinutes(secondsRemaining),
+				tasks: [...prevState.tasks, newTask],
+			};
+		});
 	}
 
 	return (
